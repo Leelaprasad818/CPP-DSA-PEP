692565fd28289109dacf0e8566e119f888785564
Leelaprasad818 <leelaprasadgavidi@gmail.com>
2026-02-06 21:37:32 +0530

    stack initialization

---
+++ Stack.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main(){
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+
+    cout<<st.top()<<end;
+    st.pop()
+    cout<<st.top()<<endl;
+    cout<<st.size()<<endl;
+}
