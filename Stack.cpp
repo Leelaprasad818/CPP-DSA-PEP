fa93503b958a1159f6ec6645806111e297c125ce
Leelaprasad818 <leelaprasadgavidi@gmail.com>
2026-02-10 09:07:44 +0530

    stack initialization

--- Stack.cpp
+++ Stack.cpp
@@ -3,14 +3,69 @@
 
 using namespace std;
 
+
+void reverseStringWords(string str){
+    stack<string> st;
+
+    for(int i=0;i<str.length();i++){
+        string word = "";
+        while(str[i] != ' ' && i < str.length()){
+            word += str[i];
+            i++;
+        }
+        st.push(word);
+    }
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }cout<<endl;
+}
+
+void reverseString(string str){
+    stack<char> st;
+
+    for(int i=0;i<str.length();i++){
+        st.push(str[i]);
+    }
+    while(!st.empty()){
+        cout<<st.top();
+        st.pop();
+    }cout<<endl;
+}
+
+
+/*
+class myStack{
+    void pop(){
+        if(top == NULL){
+            Node* temp = top;
+
+        }
+    }
+}*/
+
+
 int main(){
+    
+    /*
     stack<int> st;
     st.push(1);
     st.push(2);
     st.push(3);
-
+    
     cout<<st.top()<<end;
     st.pop()
     cout<<st.top()<<endl;
     cout<<st.size()<<endl;
+    */
+
+    cout<<endl;
+    string str = "Hello, how are you?";
+    cout<<"reversed string words of string : "<<endl;
+    reverseStringWords(str);
+    cout<<endl;
+    cout<<"reversed string : "<<endl;
+    reverseString(str);
+
+
 }
