9caa9d61dd50e54bbd065f8b6c5a6747d7438aae
Leelaprasad818 <leelaprasadgavidi@gmail.com>
2026-02-13 23:16:24 +0530

    added queue problems

--- queue.cpp
+++ queue.cpp
@@ -2,6 +2,55 @@
 
 using namespace std;
 
+/*
+https://leetcode.com/problems/design-circular-queue/
+class MyCircularQueue {
+public:
+    vector<int> data;
+    int front,rear,size,count;
+    MyCircularQueue(int k) {
+        front = 0;
+        rear = -1;
+        count = 0;
+        size = k;
+        data.clear();
+        data.resize(k);
+    }
+    
+    bool enQueue(int value) {
+        if(isFull()) return false;
+        rear = (rear+1)%size;
+        data[rear] = value;
+        count++;
+        return true;
+    }
+    
+    bool deQueue() {
+        if(isEmpty()) return false;
+        front = (front+1)%size;
+        count--;
+        return true;
+    }
+    
+    int Front() {
+        if(isEmpty()) return -1;
+        return data[front];
+    }
+    
+    int Rear() {
+        if(isEmpty()) return -1;
+        return data[rear];
+    }
+    
+    bool isEmpty() {
+        return count == 0;
+    }
+    
+    bool isFull() {
+        return count == size;
+    }
+};
+*/
 
 
 
