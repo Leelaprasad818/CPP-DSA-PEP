bea8220a8d93a3bdf14f9f48259fa41b662e4491
Leelaprasad818 <leelaprasadgavidi@gmail.com>
2026-02-23 22:15:57 +0530

    priority queue added problems

--- priorityQueue.cpp
+++ priorityQueue.cpp
@@ -13,6 +13,115 @@ internally
 
 */
 
+
+/*
+https://www.geeksforgeeks.org/problems/is-binary-tree-heap/1
+class Solution {
+  public:
+    
+    int count(Node* root){
+        if(root == nullptr) return 0;
+        return 1 + count(root->left) + count(root->right);
+    }
+    
+    bool isCBT(Node* root,int idx,int count){
+        if(root == nullptr) return true;
+        
+        if(idx >= count) return false;
+        else{
+            bool left = isCBT(root->left,2*idx+1,count);
+            bool right = isCBT(root->right,2*idx+2,count);
+            
+            return (left && right);
+        }
+    }
+    
+    bool isMaxHeap(Node* root){
+        if(root->left == nullptr && root->right == nullptr) return true;
+        
+        if(root->right == nullptr) return (root->data > root->left->data);
+        else{
+            bool left = isMaxHeap(root->left);
+            bool right = isMaxHeap(root->right);
+            
+            return((left && right ) &&root->data > root->left->data && root->data > root->right->data);     
+        }
+    }
+  
+    bool isHeap(Node* tree) {
+        // code here
+        int idx = 0;
+        int totCount = count(tree);
+        if(isCBT(tree,idx,totCount) && isMaxHeap(tree)){
+            return true;
+        }
+        return false;
+    }
+};
+*/
+
+
+/*
+https://leetcode.com/problems/top-k-frequent-elements/
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> m;
+
+        for(int i : nums){
+            m[i]++;
+        }
+
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+
+        for(auto i : m){
+            pq.push({i.second,i.first});
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+
+        vector<int> ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
+*/
+
+/*
+https://leetcode.com/problems/find-k-closest-elements/
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int> ans;
+
+        priority_queue<pair<int,int>> heap;
+
+        for(int i : arr){
+            int d = abs(i-x);
+            heap.push({d,i});
+            if(heap.size() > k){
+                heap.pop();
+            }
+        }
+
+
+        while(!heap.empty()){
+            ans.push_back(heap.top().second);
+            heap.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
+*/
+
 /*
 vector<vector<int>> diagonalWaveCollector(vector<vector<int>> &grid) {
     
