4c5c005a883cb1c30eb8397e02b27b6660daeabe
Leelaprasad818 <leelaprasadgavidi@gmail.com>
2026-03-03 23:17:18 +0530

    added graph problem

--- graphs.cpp
+++ graphs.cpp
@@ -20,6 +20,67 @@ graph G = (V,E) consist of :
 */
 
 
+
+
+/*
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution{
+public:
+    vector<vector<int>> nearestZero(vector<vector<int>>& grid){
+        // Write you code here...
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<vector<int>> ans(m,vector<int> (n,0));
+        vector<vector<int>> visited(m,vector<int>(n,0));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 0){
+                    q.push({{i,j},0});
+                    visited[i][j] = 1;
+                    ans[i][j] = 0;
+                }
+            }
+        }
+        
+        while(!q.empty()){
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            int d = q.front().second;
+            q.pop();
+            
+            if(x-1 >= 0 && !visited[x-1][y]){
+                q.push({{x-1,y},d+1});
+                ans[x-1][y] = d+1;
+                visited[x-1][y] = 1;
+            }
+            if(x+1 < m && !visited[x+1][y]){
+                q.push({{x+1,y},d+1});
+                ans[x+1][y] = d+1;
+                visited[x+1][y] = 1;
+            }
+            if(y-1 >= 0 && !visited[x][y-1]){
+                q.push({{x,y-1},d+1});
+                ans[x][y-1] = d+1;
+                visited[x][y-1] = 1;
+            }
+            if(y+1 < n && !visited[x][y+1]){
+                q.push({{x,y+1},d+1});
+                ans[x][y+1] = d+1;
+                visited[x][y+1] = 1;
+            }
+            
+        }
+        return ans;
+    }
+};
+*/
+
+
 /*
 
 https://www.geeksforgeeks.org/problems/shortest-path-in-undirected-graph/1
